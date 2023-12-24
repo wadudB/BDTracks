3d94d1349a6d5ac95c7f1518ce33068f304829b5
wadudB <wadudbillah@gmail.com>
2023-12-24 13:20:18 +0600

    selector fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -119,7 +119,7 @@ const Dashboard = () => {
     const selectedYearData = accidentData.find((item) => item.year === newYear);
 
     if (selectedYearData) {
-      // console.log("Selected year", selectedYearData);
+      console.log("Selected year", selectedYearData);
       setTotalAccidents(selectedYearData.total_accidents);
       setTotalDeaths(selectedYearData.total_killed);
       setTotalInjured(selectedYearData.total_injured);
@@ -147,7 +147,7 @@ const Dashboard = () => {
         style={{
           display: "flex",
           justifyContent: "space-between",
-          alignItems: "right",
+          alignItems: "center",
         }}
       >
         <Typography
@@ -163,55 +163,59 @@ const Dashboard = () => {
         >
           Road Accident Dashboard
         </Typography>
-        <Select
-          value={selectedYear}
-          onChange={handleYearChange}
-          sx={{
-            marginLeft: 2,
-            width: 100,
-            color: "white",
-            textAlign: "right",
-            borderColor: "white",
-            borderWidth: 0.1,
-            fontSize: {
-              xs: "0.875rem", // smaller font size on extra-small screens
-              sm: "1rem", // default font size on small screens and up
-            },
-            backgroundColor: "#202940",
-            borderStyle: "solid",
-            "& .MuiSelect-select": {
-              padding: "6px",
-            },
-            "& .MuiSvgIcon-root": {
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Select
+            value={selectedYear}
+            onChange={handleYearChange}
+            sx={{
+              marginLeft: 2,
+              width: 100,
               color: "white",
-              fontSize: "1.25rem",
-            },
-          }}
-          MenuProps={{
-            PaperProps: {
-              style: {
-                backgroundColor: "#202940",
+              textAlign: "right",
+              borderColor: "white",
+              borderWidth: 0.1,
+              fontSize: {
+                xs: "0.875rem", // smaller font size on extra-small screens
+                sm: "1rem", // default font size on small screens and up
+              },
+              backgroundColor: "#202940",
+              borderStyle: "solid",
+              "& .MuiSelect-select": {
+                // Style for the select input
+                padding: "6px",
+              },
+              "& .MuiSvgIcon-root": {
+                // Style for the dropdown icon
                 color: "white",
+                fontSize: "1.25rem",
               },
-            },
-            sx: {
-              ".MuiMenuItem-root": {
-                justifyContent: "center",
+            }}
+            MenuProps={{
+              PaperProps: {
+                style: {
+                  backgroundColor: "#202940",
+                  color: "white",
+                },
               },
-            },
-          }}
-        >
-          {/* Menu items */}
-          {getUniqueYears().map((year) => (
-            <MenuItem
-              key={year}
-              value={year}
-              style={{ justifyContent: "center" }}
-            >
-              {year}
-            </MenuItem>
-          ))}
-        </Select>
+              sx: {
+                ".MuiMenuItem-root": {
+                  justifyContent: "center",
+                },
+              },
+            }}
+          >
+            {/* Menu items */}
+            {getUniqueYears().map((year) => (
+              <MenuItem
+                key={year}
+                value={year}
+                style={{ justifyContent: "center" }}
+              >
+                {year}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
       </div>
       <div className="my-5">
         <Divider variant="fullwidth" sx={{ borderColor: "#ffffff" }} />
