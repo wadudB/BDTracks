d8235b33e4eea66b45503ff08cd0d492882ccc54
wadudB <wadudbillah@gmail.com>
2024-03-24 10:12:33 +0600

    added scrollbar design

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./css/scrollbar-styles.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -12,6 +13,11 @@ const theme = createTheme({
       '"Lucida Console"',
       "monospace",
     ].join(","),
+  },
+  palette: {
+    text: {
+      primary: "#CBD5E1",
+    },
   },
 });
 
